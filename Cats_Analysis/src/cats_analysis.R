15a5370f0bcebe462d4d0c512a930a4a55fef546
rene <rene.ortiz.350f@gmail.com>
2024-09-01 09:41:05 -0700

    added structure for cat analysis

---
+++ Cats_Analysis/src/cats_analysis.R
@@ -0,0 +1,25 @@
+# install packages
+
+# load packages
+
+# import dataset
+
+# verify data
+
+# checks: duplicates, N/A
+
+## Clean Data ##
+
+# remove unnecessary
+
+# clean column names
+
+# change formats
+
+# export clean dataset
+
+## Analysis ##
+
+# summary
+
+# visualizations
