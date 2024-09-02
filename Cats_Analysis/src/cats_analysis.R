a8691705c7c0f3c4286e122381c0e7813d02279f
r <rene.ortiz.350f@gmail.com>
2024-09-02 15:00:43 -0700

    install.packages and add output directory

--- Cats_Analysis/src/cats_analysis.R
+++ Cats_Analysis/src/cats_analysis.R
@@ -1,4 +1,12 @@
 # install packages
+install.packages("dplyr")
+install.packages("ggeasy")
+install.packages("ggplot2")
+install.packages("ggrepel")
+install.packages("janitor")
+install.packages("lubridate")
+install.packages("skimr")
+install.packages("tidyverse")
 
 # load packages
 
